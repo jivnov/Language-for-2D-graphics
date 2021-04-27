dd7b7aadf1cefcfe91c7d861fe85e82b2a92d01f
Pablo1785 <pablo1785@wp.pl>
2021-04-27 20:25:30 +0200

    Rewrite to svgwrite

--- drawing.py
+++ drawing.py
@@ -1,20 +1,21 @@
 import sys
-import drawSvg as d
+import svgwrite as svg
 from graph import Shape, Vertex
+
 
 class Drawing2d:
     def __init__(self, w, h):
-        self.viewport_width = w 
+        """
+
+        :param w:
+        :param h:
+        """
+        self.viewport_width = w
         self.viewport_height = h
-        self.canvas = d.Drawing(w, h, origin="center")
+        self.canvas = svg.Drawing(size=(w, h), filename='generated_images/svgwrite_example.svg')
 
     def draw(self, v: Vertex):
-        if (v.shape == Shape.RECT):
-            x = -1 * v.bb_w * self.viewport_width /2
-            y = -1 * v.bb_h * self.viewport_height /2
-            r = d.Rectangle(x, y, v.bb_w*self.viewport_width, v.bb_h * self.viewport_height)
-            self.canvas.append(r)
-
-            
-
-    
+        if v.shape == Shape.RECT:
+            x = -1 * v.bb_w * self.viewport_width / 2
+            y = -1 * v.bb_h * self.viewport_height / 2
+            self.canvas.add(self.canvas.rect(insert=(x, y), size=(v.bb_w * self.viewport_width, v.bb_h * self.viewport_height)))
--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from typing import Any
 
+
 class Shape(Enum):
     SQUARE = 1
     CIRCLE = 2
@@ -8,17 +9,19 @@ class Shape(Enum):
     TRIANGLE = 4
     SHAPE = 5
 
+
 class Relation(Enum):
     LEFT = 1
     RIGHT = 2
-    TOP = 3 
+    TOP = 3
     BOT = 4
     IN = 5
     ON = 6
     UNDER = 7
 
+
 class Vertex:
-    def __init__(self, var_name: str,  shape: str,  args: Any):
+    def __init__(self, var_name: str, shape: str, args: Any):
         self.name = var_name
         self.shape = None
         self.bb_w = 0.0
@@ -31,28 +34,29 @@ class Vertex:
         if shape == "triangle":
             self.shape = Shape.TRIANGLE
         if shape == "shape":
-            self.shape == Shape.SHAPE
-        if (isinstance(args, list) and len(args)>0):
-            self.bb_w = int(args[0].replace('%', ''))/100
-            self.bb_h = int(args[1].replace('%', ''))/100
+            self.shape = Shape.SHAPE
+        if isinstance(args, list) and len(args) > 0:
+            self.bb_w = int(args[0].replace('%', '')) / 100
+            self.bb_h = int(args[1].replace('%', '')) / 100
+
 
 class Graph:
     def __init__(self):
         self.vertices = {}
 
     def add_vertex(self, v: Vertex):
-        if (v not in self.vertices.keys()):
+        if v not in self.vertices.keys():
             self.vertices[v] = []
 
     def add_edge(self, v_from: Vertex, v_to: Vertex, r: Relation):
-        if (v_from not in self.vertices.keys()):
-            add_vertex(v_from)
-        if (v_to not in self.vertices.keys()):
-            add_vertex(v_to)
+        if v_from not in self.vertices.keys():
+            self.add_vertex(v_from)
+        if v_to not in self.vertices.keys():
+            self.add_vertex(v_to)
         self.vertices[v_from].append([v_to, r])
 
     def get_relations(self, v: Vertex):
-        #TODO generate SVG for provided parameters
+        # TODO generate SVG for provided parameters
         if v not in self.vertices:
             print("not in graph")
         else:
@@ -61,9 +65,8 @@ class Graph:
                 vertex_name = el[0].name
                 vertex_relation = el[1]
                 print(f"{el[0].shape}:{vertex_name} {vertex_relation}")
-    
+
     def find_vertex(self, vertex_name: str) -> Vertex:
         for v in self.vertices.keys():
             if str(v.name) == str(vertex_name):
                 return v
-
