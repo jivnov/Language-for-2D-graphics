882352c04f74071905e7f5bb1ba6f0129ca193ef
Lizaveta Hurskaya <lizavetahurskaya@gmail.com>
2021-06-18 11:58:54 +0200

    Syntax error modified

--- main.py
+++ main.py
@@ -12,33 +12,47 @@ from TwoDimParser import TwoDimParser
 import logging
 import os
 
+
 class SyntaxErrorListener(ErrorListener):
     def __init__(self):
         super(SyntaxErrorListener, self).__init__()
 
     def syntaxError(self, recognizer, offending_symbol, line, column, msg, e):
-        if e.input.tokens[offending_symbol.tokenIndex-1].text.upper() in ["RIGHT", "LEFT", "TOP", "BOT", "IN"] or (e.input.tokens[offending_symbol.tokenIndex-1].text.upper()==" " and e.input.tokens[offending_symbol.tokenIndex-2].text.upper() in ["RIGHT", "LEFT", "TOP", "BOT", "IN"]):
-            raise SyntaxError(f"Syntax error in relational operation at line {line} column {column}. Check the operation declaration.")
-        raise SyntaxError(f"Symbol \"{offending_symbol.text}\" at line {line} column {column} not recognized. \n"
-                          f"Tip: If \"{offending_symbol.text}\" is what you meant here, check if preceding symbols"
-                          f" are also correct and conforms with syntax. Also check if there is a semicolon in "
-                          f"the preceding line.")  # e.input.tokenSource.inputStream.strdata[:offending_symbol.start]
+        try:
+            if e.input.tokens[offending_symbol.tokenIndex - 1].text.upper() in ["RIGHT", "LEFT", "TOP", "BOT", "IN"] or (
+                    e.input.tokens[offending_symbol.tokenIndex - 1].text.upper() == " " and e.input.tokens[
+                offending_symbol.tokenIndex - 2].text.upper() in ["RIGHT", "LEFT", "TOP", "BOT", "IN"]):
+                raise SyntaxError(
+                    f"Syntax error in relational operation at line {line} column {column}. Check the operation declaration.")
+            raise SyntaxError(f"Symbol \"{offending_symbol.text}\" at line {line} column {column} not recognized. \n"
+                              f"Tip: If \"{offending_symbol.text}\" is what you meant here, check if preceding symbols"
+                              f" are also correct and conforms with syntax. Also check if there is a semicolon in "
+                              f"the preceding line.")
+        except Exception:
+            raise SyntaxError(f"At line {line} column {column}: {msg}.")
+
 
 class LexerErrorListener(ErrorListener):
     def __init__(self):
         super(LexerErrorListener, self).__init__()
 
     def syntaxError(self, recognizer, offending_token, line, column, msg, e):
-        endIndex = e.input.index
-        offset = len(str(e.input.strdata)) - e.input.index
-        for c in [' ', ';', '(', ')', '[', ']', '{', '}', '<', '>', ',']:
-            if str(e.input.strdata)[e.input.index:].find(c) >= 0 and str(e.input.strdata)[e.input.index:].find(c) < offset:
-                offset = str(e.input.strdata)[e.input.index:].find(c)
-        endIndex += offset
+        try:
+            endIndex = e.input.index
+            offset = len(str(e.input.strdata)) - e.input.index
+            for c in [' ', ';', '(', ')', '[', ']', '{', '}', '<', '>', ',']:
+                if str(e.input.strdata)[e.input.index:].find(c) >= 0 and str(e.input.strdata)[e.input.index:].find(
+                        c) < offset:
+                    offset = str(e.input.strdata)[e.input.index:].find(c)
+            endIndex += offset
 
-        offending_token = str(e.input.strdata)[e.startIndex:endIndex]
-        raise SyntaxError(f"Syntax Error (Incorrect lexeme): failed to parse token \"{offending_token}\" at line {line} column {column}."
-                          f"Check if it fits in syntax rules, is typed correctly and the register used is appropriate.")
+            offending_token = str(e.input.strdata)[e.startIndex:endIndex]
+            raise SyntaxError(
+                f"Syntax Error (Incorrect lexeme): failed to parse token \"{offending_token}\" at line {line} column {column}."
+                f"Check if it fits in syntax rules, is typed correctly and the register used is appropriate.")
+        except Exception:
+            raise SyntaxError(
+                f"Syntax Error (Incorrect lexeme): failed to parse a token at line {line} column {column}.")
 
 
 def main(argv):
@@ -78,9 +92,10 @@ def main(argv):
 
         # Create listener
         if len(argv) >= 3:  # If output path was specified
-            printer = SecondPassTwoDimParserListener(global_context=global_context, parser = parser, output_path=argv[2])
+            printer = SecondPassTwoDimParserListener(global_context=global_context, parser=parser, output_path=argv[2])
         else:
-            printer = SecondPassTwoDimParserListener(global_context=global_context, parser = parser, output_path='generated_images/output.svg')
+            printer = SecondPassTwoDimParserListener(global_context=global_context, parser=parser,
+                                                     output_path='generated_images/output.svg')
 
         # Walk the generated tree with our listener attached
         walker = ParseTreeWalker()
@@ -95,6 +110,7 @@ def main(argv):
         message = f"File {'' if len(argv) < 2 else os.path.abspath(argv[1])}\n{e}"
         exception_type = e.__class__
         logging.error(message)
+
 
 if __name__ == '__main__':
     try:
